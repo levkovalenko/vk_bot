90209e83c5111af1fbea114c9a76d72ed01bda5f
korwin <levozavr@mail.ru>
2018-05-17 11:32:01 +0300

    Some changes

--- teat_api.py
+++ teat_api.py
@@ -78,7 +78,7 @@ def getNewPostsId(vkApi, session):
 
 
 def main():
-    session = vk.AuthSession('5988829', '79163064478', 'popapopa', scope='wall,photos')
+    session = vk.AuthSession('5988829', '79163064478', 'popapopa', scope='wall,photos,messages')
     vk_api = vk.API(session)
     i = 0
     while True:
@@ -88,10 +88,11 @@ def main():
             getNewPostsId(vk_api, session)
             time.sleep(300)
             if i % 16 == 0:
-                vk_api.wall.post(owner_id='465137130', message='work', v=5.52)
+                vk_api.messages.send(domain='itchert', message='work', v=5.52)
+            time.sleep(300)
 
         except:
-            vk_api.wall.post(owner_id='465137130', message='broke', v=5.52)
+            vk_api.messages.send(domain='itchert', message='lol', v=5.52)
 
 
 
